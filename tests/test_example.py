5d7e607af097be413d4be772cae25c92b5941fe4
alex.fayers <alex@fayers.com>
2022-11-03 18:42:24 +0000

    add an example test

---
+++ tests/test_example.py
@@ -0,0 +1,9 @@
+"""Example tests for new_project_readable_name."""
+
+
+def test_main() -> None:
+    """Test the main function.
+
+    This actually does nothing, and is just a placeholder.
+    """
+    assert True
