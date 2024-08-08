dc98bf9def360f34c8417633e3c9729b28f10408
alexfayers <alex@fayers.com>
2024-08-08 21:08:30 +0100

    update various things about main repo from template section

--- scripts/export_requirements.sh
+++ scripts/export_requirements.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-# Ensure that the poetry-plugin-export plugin is installed
-if ! poetry self show plugins | grep -q "poetry-plugin-export"; then
-    echo "[!] Please install the poetry-plugin-export poetry plugin before continuing..."
-    exit 1
-else
-    if poetry config warnings.export | grep -q "true"; then
-        echo "[-] poetry-plugin-export warnings are enabled. Consider disabling them because they can be noisy (poetry config warnings.export false)."
-    fi
-fi
-
 # Ensure the poetry lock file is up to date
 echo "[-] Making sure lock file is up to date..."
 
 poetry lock
-# git add poetry.lock
-# git commit -m "Update poetry.lock"
+git add poetry.lock
+git commit -m "Update poetry.lock"
 
 # Export the base project requirements poetry into requirements.txt format.
 echo "[-] Exporting main requirements..."
@@ -38,5 +28,7 @@ done
 
 echo "[+] All requirements exported."
 
-# git add "requirements*.txt"
-# git commit -m "Update requirements files"
+git add "requirements*.txt"
+git commit -m "Update requirements files"
+
+exit 0
